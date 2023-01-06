54ec07c56a0fcff7d15a2e37cf5ce6735cbfcb3f
adriendavin <adriendavin@hotmail.fr>
2023-01-06 18:20:56 +0100

    dialog box js

---
+++ exemple.js
@@ -0,0 +1,17 @@
+// console.log("toto");
+
+//BOITE DE DIALOGUE
+
+if(confirm("Tu es sûr ?")){
+  //oui
+  alert('ok');
+}
+else{
+  //non
+  alert('annulation');
+}
+
+
+// const age = prompt("Enter your age :")
+// alert("Bonjour, vous avez " + age + " ans.")
+// console.log("Bonjour, vous avez " + age + " ans");
